5dc03b60c5c27b22b21ab245b1aa1aed39192d30
L-jy16 <lee.jiyoung492@gmail.com>
2023-10-31 11:13:39 +0900

    2023.10.31 수정2

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -41,17 +41,17 @@ export default {
     return {
       cardInfo: [
         {
-          img: "../assets/img/img05.jpg",
+          img: "/src/assets/img/img05.jpg",
           title: "웹표준 사이트 만들기",
           desc: "사이트를 만들기 위한 기초 공부입니다. 기본 코딩에 대한 감각을 익히고, 웹 표준을 준수하면서 코딩하는 방법을 익힙니다. 다양한 예제를 통해서 사이트 만드는 기본을 익히도록 합니다."
         },
         {
-          img: "../assets/img/img06.jpg",
+          img: "/src/assets/img/img06.jpg",
           title: "반응형 사이트 만들기",
           desc: "웹표준 사이트를 완성하고, 반응형을 익히는 사이트입니다. 기본 코딩을 익히고, 반응형 감각을 키우면 모바일 및 반응형 사이트도 만들 수 있습니다."
         },
         {
-          img: "../assets/img/img07.jpg",
+          img: "/src/assets/img/img07.jpg",
           title: "페랠렉스 사이트 만들기",
           desc: "기업 사이트 중 50% 이상이 패랙렉스 스크로링 효과로 만들어져 있습니다. 웹 표준, 반응형 사이트를 완성하였다면, 이제는 인터랙티브한 패랠렉스 스크롤링 사이트를 완성할 수 있습니다."
         }
